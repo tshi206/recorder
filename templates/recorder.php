aaabe28f8e4b9a07d8c996d0d6bce6444b8332a4
YsqEvilmax <ysq.evilmax@qq.com>
2016-04-24 22:06:50 +1200

    test style

--- templates/recorder.php
+++ templates/recorder.php
@@ -1,12 +1,3 @@
-<?php
-<script type="text/javascript" src="https://cdn.webrtc-experiment.com/MediaStreamRecorder.js"></script>
-<script type="text/javascript" src="https://cdn.rawgit.com/webrtc/adapter/master/adapter.js"></script>
-script('recorder', 'audiodisplay');
-script('recorder', 'main');
-script('recorder', 'recorder');
-style('recorder', 'recorder');
-?>
-
 <div>
     <button id="start-recording" onclick="toggleRecording(this);">Record</button>
     <button id="stop-recording" disabled>Stop Recording</button>
