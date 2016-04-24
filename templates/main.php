8ba53a33bd88d437d323d80d59c1032b52610ec9
Shawn <ysq.evilmax@qq.com>
2016-04-24 22:20:38 +1200

    test js

--- templates/main.php
+++ templates/main.php
@@ -2,9 +2,9 @@
 script('recorder', 'script');
 style('recorder', 'style');
 
-/*<script type="text/javascript" src="https://cdn.webrtc-experiment.com/MediaStreamRecorder.js"></script>
+/*<script type="text/javascript" src="https://cdn.webrtc-experiment.com/MediaStreamRecorder.js"></script>*/
 <script type="text/javascript" src="https://cdn.rawgit.com/webrtc/adapter/master/adapter.js"></script>
-script('recorder', 'audiodisplay');*/
+script('recorder', 'audiodisplay');
 script('recorder', 'main');
 script('recorder', 'recorder');
 style('recorder', 'recorder');
