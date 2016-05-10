6d70d8b38db875ad2dab79ca1af112b67fa1909f
Ahmad Khudeish <ahmadkhudeish@gmail.com>
2016-05-10 21:47:51 +1200

    Added Recorder Navigation

--- templates/recorder.php
+++ templates/recorder.php
@@ -1,4 +1,5 @@
 <div>
+<h1>Recorder Navigation</h1>
     <button id="start-recording" onclick="toggleRecording(this);">Record</button>
     <button id="stop-recording" disabled>Stop Recording</button>
     <button id="save-recording" disabled>Save Recording</button>
