7aeadd0dfd9c7a7f58fe4ce0d2e4748a0529c3a7
Ahmad Khudeish <ahmadkhudeish@gmail.com>
2016-05-13 08:36:05 +1200

    no message

--- templates/main.php
+++ templates/main.php
@@ -1,24 +1,3 @@
-<?php
-script('recorder', 'script');
-style('recorder', 'style');
-script('recorder', 'MediaStreamRecorder');
-// script('recorder', 'audiodisplay');
-script('recorder', 'main');
-// script('recorder', 'recorder');
-style('recorder', 'recorder');
-?>
-
-<div id="app">
-	<div id="app-navigation">
-		<?php print_unescaped($this->inc('part.navigation')); ?>
-		<?php print_unescaped($this->inc('part.settings')); ?>
-	</div>
-
-	<div id="app-content">
-		<div id="app-content-wrapper">
-			<?php print_unescaped($this->inc('recorder')); ?>
-		</div>
-	</div>
 
 	<script>
 	    function captureUserMedia(mediaConstraints, successCallback, errorCallback) {
@@ -96,5 +75,27 @@ style('recorder', 'recorder');
     };
 
 </script>
+
+<?php
+script('recorder', 'script');
+style('recorder', 'style');
+script('recorder', 'MediaStreamRecorder');
+// script('recorder', 'audiodisplay');
+script('recorder', 'main');
+// script('recorder', 'recorder');
+style('recorder', 'recorder');
+?>
+
+<div id="app">
+	<div id="app-navigation">
+		<?php print_unescaped($this->inc('part.navigation')); ?>
+		<?php print_unescaped($this->inc('part.settings')); ?>
+	</div>
+
+	<div id="app-content">
+		<div id="app-content-wrapper">
+			<?php print_unescaped($this->inc('recorder')); ?>
+		</div>
+	</div>
 
 </div>
