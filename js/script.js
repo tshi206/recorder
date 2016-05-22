890d487e5004a1dc4821d69d5a64583b610c6c70
Ahmad Khudeish <ahmadkhudeish@gmail.com>
2016-05-22 20:26:00 +1200

    no message

--- js/script.js
+++ js/script.js
@@ -35,38 +35,38 @@
                 audio: true
             };
 
-            $('#start-recording').onclick = function() {
+            $('#start-recording').click(function() {
                 this.disabled = true;
                 captureUserMedia(mediaConstraints, onMediaSuccess, onMediaError);
-            };
+            });
 
-            $('#stop-recording').onclick = function() {
+            $('#stop-recording').click(function() {
                 this.disabled = true;
                 mediaRecorder.stop();
                 mediaRecorder.stream.stop();
 
                 $('#pause-recording').disabled = true;
                 $('#start-recording').disabled = false;
-            };
+            });
 
-            $('#pause-recording').onclick = function() {
+            $('#pause-recording').click(function() {
                 this.disabled = true;
                 mediaRecorder.pause();
 
                 $('#resume-recording').disabled = false;
-            };
+            });
 
-            $('#resume-recording').onclick = function() {
+            $('#resume-recording').click(function() {
                 this.disabled = true;
                 mediaRecorder.resume();
 
                 $('#pause-recording').disabled = false;
-            };
+            });
 
-            $('#save-recording').onclick = function() {
+            $('#save-recording').click(function() {
                 this.disabled = true;
                 mediaRecorder.save();
-            };
+            });
 
             var mediaRecorder;
 
