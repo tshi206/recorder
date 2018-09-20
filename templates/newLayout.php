29ff08e60b9653f82e42f942382171d14f4554d6
Mason <tshi206@aucklanduni.ac.nz>
2018-09-20 18:28:13 +1200

    Try place modal at a higher level

--- templates/newLayout.php
+++ templates/newLayout.php
@@ -66,24 +66,3 @@
         </div>
     </div>
 </div>
-<!-- Modal -->
-<div class="modal fade" id="fenetre_alert" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalCenterTitle">Thank you!</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                Thank you very much for contributing to Voices of New Zealand!
-                Your uploaded file will be saved as "#your-user-id#_#timestamp#_#some-generated-fileName#.wav". When the upload successfully completes, you will see a new window containing your files. Please DO NOT close the recorder application (the web page) before your upload completes.
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button id="done" type="button" class="btn btn-primary">Start uploading</button>
-            </div>
-        </div>
-    </div>
-</div>
